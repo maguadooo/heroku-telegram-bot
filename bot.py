8db583c1a004e9e11c5261bd9aac951e77f6400c
maguadooo <maguado1992@gmail.com>
2018-08-02 17:13:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,6 @@ import telebot
 # Example of your code beginning
 #           Config vars
 token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
 #             ...
 
 bot = telebot.TeleBot("TOKEN")
